f70289aca5e7f0fd5ec66438b9dc52a769e35390
jbergen <jbergen@gmail.com>
2018-05-24 00:14:45 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
 import Page from './Pages/Page';
 import ProjectListItem from './Projects/ProjectListItem';
 import data from './cms/data.json';
 import _ from 'lodash';
 import './bootstrap/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
     constructor(props) {
@@ -22,7 +24,7 @@ class App extends Component {
 
         return (
             <div className='container'>
-                <h1>{this.state.data.site_info.title}</h1>
+                <NavBar/>
                 <Page data={this.state.data.pages[0]}/>
                 <ul>
                     { projectListItems }
