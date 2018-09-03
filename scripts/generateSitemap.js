2b564f779719577275ef178535c110f219ec3b5b
Joseph Bergen <jbergen@gmail.com>
2018-09-03 00:57:04 -0400

    adds sitemap stuff

---
+++ scripts/generateSitemap.js
@@ -0,0 +1,59 @@
+const baseURL = 'http://jbergen.github.io/';
+
+// Read Synchrously
+const fs = require("fs");
+const dataString = fs.readFileSync("src/cms/data.json");
+const data = JSON.parse(dataString);
+
+const sitemapArray = [];
+sitemapArray.push('<?xml version="1.0" encoding="UTF-8"?>');
+sitemapArray.push('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
+
+data.pages.forEach(page => {
+    if (!page.hide_in_nav) {
+        sitemapArray.push('  <url>');
+        sitemapArray.push(`    <loc>${baseURL}${page.name}</loc>`)
+        sitemapArray.push('  </url>');
+
+        if (page.name === 'home') {
+            page.media.forEach(mediaId => {
+                const media = data.media.find(m => mediaId === m.id);
+                if (media) {
+                    sitemapArray.push('  <url>');
+                    sitemapArray.push(`    <loc>${baseURL}media/${media.id}</loc>`)
+                    sitemapArray.push(`    <image:image>`)
+                    sitemapArray.push(`      <image:loc>${baseURL}static/media/${media.filename}</image:loc>`)
+                    sitemapArray.push(`      <image:title>${media.title}</image:title>`)
+                    sitemapArray.push(`    </image:image>`)
+                    sitemapArray.push('  </url>');
+                }
+            });
+        }
+    }
+});
+
+data.posts.forEach(post => {
+    if (!post.visible) {
+        sitemapArray.push('  <url>');
+        sitemapArray.push(`    <loc>${baseURL}work/${post.name}</loc>`)
+        sitemapArray.push('  </url>');
+    }
+});
+
+sitemapArray.push('</urlset>');
+
+
+
+// console.log(data.pages);
+
+// console.log("Output Content : \n"+ data);
+// console.log("\n *EXIT* \n");
+
+
+fs.writeFile("public/sitemap.xml", sitemapArray.join('\r\n'), err => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
