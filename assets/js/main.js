2307fd89cfea445adff49643c4ff62a07c423520
jbergen <jbergen@gmail.com>
2019-02-09 12:05:45 -0500

    js cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 class JBergen {
     constructor() {
+        console.log("✨🚀🌝✨");
         this.startMasonry();
     }
 
@@ -26,90 +27,12 @@ class JBergen {
     }
 
     masonryOptions(isMobile) {
-        if (isMobile) {
-            return {
-                percentPosition: true,
-                columnWidth: '.column-width-1',
-                gutter: 10,
-            }
-        } else {
-            return {
-                horizontalOrder: false,
-                columnWidth: 100,
-                gutter: 10,
-            }
-        }
-    }
-
-    onSwipe(direction) {
-        switch (direction) {
-        case "left":
-            console.log("GO NEXT", window.siteData);
-            if (window.siteData.nextURL) {
-                window.location.href = window.siteData.nextURL;
-            }
-            break;
-        case "right":
-            console.log("GO PREV");
-            if (window.siteData.prevURL) {
-                window.location.href = window.siteData.prevURL;
-            }
-            break;
-        }
-    }
-
-    updateNext(nextURL) {
-        this.nextURL = nextURL;
-    }
-
-    updatePrev(prevURL) {
-        this.prevURL = prevURL;
-    }
-
-    // credit: http://www.javascriptkit.com/javatutors/touchevents2.shtml
-    swipeDetect(el, callback) {
-        var touchsurface = el,
-        swipedir,
-        startX,
-        startY,
-        distX,
-        distY,
-        threshold = 150, //required min distance traveled to be considered swipe
-        restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-        allowedTime = 300, // maximum time allowed to travel that distance
-        elapsedTime,
-        startTime,
-        handleswipe = callback || function(swipedir){}
-    
-        touchsurface.addEventListener('touchstart', e => {
-            var touchobj = e.changedTouches[0]
-            swipedir = 'none'
-            startX = touchobj.pageX
-            startY = touchobj.pageY
-            startTime = new Date().getTime() // record time when finger first makes contact with surface
-            e.preventDefault()
-        }, false)
-    
-        touchsurface.addEventListener('touchmove', e => {
-            e.preventDefault() // prevent scrolling when inside DIV
-        }, false)
-    
-        touchsurface.addEventListener('touchend', e => {
-            var touchobj = e.changedTouches[0]
-            distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-            distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-            elapsedTime = new Date().getTime() - startTime // get time elapsed
-            if (elapsedTime <= allowedTime) { // first condition for awipe met
-                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                    swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-                }
-                else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                    swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-                }
-            }
-            handleswipe(swipedir)
-            e.preventDefault()
-        }, false)
+        return {
+            columnWidth: '.column-width-1',
+            gutter: 10,
+            horizontalOrder: false,
+            percentPosition: isMobile,
+        };
     }
 
     isMobile() {
