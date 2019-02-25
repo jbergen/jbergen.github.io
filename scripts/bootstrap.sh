84a1cf700f5844128c52dd0955d3b70193fe7f8a
jbergen <jbergen@gmail.com>
2019-02-24 23:19:05 -0500

    better compression

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 echo "bootstrap website"
 
+IMG_DIR=assets/img
+
+generate () {
+    echo "👌 Resizing: directory: $1, scale: $2"
+    # pwd
+    SUB_DIR=$IMG_DIR/$1
+    cp -R media $SUB_DIR
+    cd $SUB_DIR
+    mogrify -resize $2 -format jpg *.png
+    mogrify -resize $2 -quality 60 -format webp *.jpg
+    rm *.png
+    # pwd
+    cd ../../..
+    # pwd
+}
+
 echo "removing old thumbnails"
-rm -rf assets/img
+rm -rf $IMG_DIR
 
 echo "creating new thumb folders"
-mkdir assets/img
-cp -R media assets/img/display
-cp -R media assets/img/thumbnails_large
-cp -R media assets/img/thumbnails_small
+mkdir $IMG_DIR
 
-echo "resizing display"
-cd assets/img/display
-mogrify -resize '1200x1200^' -format webp *.jpg
-mogrify -resize '1200x1200^' -format webp *.png
+generate display '1200x1200^'
+generate thumbnails_large '500x500^'
+generate thumbnails_small '250x250^'
+
+
+
+# echo "resizing display"
+# cd assets/img/display
+# mogrify -resize '1200x1200^' - quality 60 -format webp *.jpg
+# # mogrify -resize '1200x1200^' -format webp *.png
+# mogrify -resize '1200x1200^' -quality 20 -format jp2 *.jpg
 # rm *.jpg
 # rm *.png
 
-echo "resizing large"
-cd ../thumbnails_large
-mogrify -resize '500x500^' -format webp *.jpg
-mogrify -resize '500x500^' -format webp *.png
-# rm *.jpg
-# rm *.png
+# echo "resizing large"
+# cd ../thumbnails_large
+# mogrify -resize '500x500^' -format webp *.jpg
+# mogrify -resize '500x500^' -format webp *.png
 
-echo "resizing small"
-cd ../thumbnails_small
-mogrify -resize '250x250^' -format webp *.jpg
-mogrify -resize '250x250^' -format webp *.png
-# rm *.jpg
-# rm *.png
+# echo "resizing small"
+# cd ../thumbnails_small
+# mogrify -resize '250x250^' -format webp *.jpg
+# mogrify -resize '250x250^' -format webp *.png
